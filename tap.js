c210d58f57af478d698e3e2a7a9552eef12aa719
yulfather <soulplus82@gmail.com>
2022-02-07 23:12:25 +0900

    이벤트 버블링 활용 탭기능1

--- tap.js
+++ tap.js
@@ -22,12 +22,31 @@
 //   $('.tab-content').eq(2).addClass('show');
 // });
 
-let tab_count = $('.tab-button').length;
-for (let i = 0; i < tab_count; i++){
-  $('.tab-button').eq(i).click(function(){
-    $('.tab-button').removeClass('active');
-    $('.tab-content').removeClass('show');
-    $('.tab-button').eq(i).addClass('active');
-    $('.tab-content').eq(i).addClass('show');
-});
+// let tab_count = $('.tab-button').length;
+// for (let i = 0; i < tab_count; i++){
+//   $('.tab-button').eq(i).click(function(){
+//     $('.tab-button').removeClass('active');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-button').eq(i).addClass('active');
+//     $('.tab-content').eq(i).addClass('show');
+// });
+// };
+
+function tabOpen(tabN){
+  $('.tab-button').removeClass('active');
+  $('.tab-content').removeClass('show');
+  $('.tab-button').eq(tabN).addClass('active');
+  $('.tab-content').eq(tabN).addClass('show');
 };
+
+$('.list').click(function(e){
+  if(e.target == document.querySelectorAll('.tab-button')[0]){
+    tabOpen(0)
+  }
+  if(e.target == document.querySelectorAll('.tab-button')[1]){
+    tabOpen(1)
+  }
+  if(e.target == document.querySelectorAll('.tab-button')[2]){
+    tabOpen(2)
+  }
+});
